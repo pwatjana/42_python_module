b4c9f44e434fb0072a5b79221637169ce23f3a6c
pwatjana <ppwj10976@gmail.com>
2024-01-30 20:57:35 +0700

    update

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 
-from services.cataas import get_cat_ids
-def download_cats()
+from services.cataas import download_cats
 
-print(get_cat_ids(3))
 
+def main():
+    download_cats(5)
+
+if __name__ == "__main__":
+    main()
+# when every we call the project in the terminal
+# these code will turn first file name into __main__
+# when we have several file.py we can see the main file which need to process first.
+# place this set of code in the main.py
--- services/cataas.py
+++ services/cataas.py
@@ -3,12 +3,27 @@ import httpx
 
 def get_cat_ids(n: int)->list[dict]:
     ret = []
-    for _ in range(n):
-        with httpx.Client() as client:
-            r = client.get("https://cataas.com/cat?json=true")
-            r.raise_for_status()
-            ret.append({
-                "id": r.json()["_id"],
-                "ext": r.json()["mimetype"].split("/")[-1]
-        })
+    with httpx.Client() as client:
+        for _ in range(n):
+                    r = client.get("https://cataas.com/cat?json=true")
+                    r.raise_for_status()
+                    ret.append({
+                        "id": r.json()["_id"],
+                        "ext": r.json()["mimetype"].split("/")[-1]
+                    })
     return ret
+
+
+def download_cats(n: int) -> None:
+    """
+    Download  a list of cats from the cataas API.
+    """
+    cat_ids = get_cat_ids(n)
+    with httpx.Client(timeout=10) as client:
+        for cat_id in cat_ids:
+            r = client.get(f"http//cataas.com/cat/{cat_id['id']}")
+            r.raise_for_status()
+            with open(f"out/cats/{cat_id['id']}.{cat_id['ext']}","wb") as f:
+                #open blank folder name out and write file content with name cat ID and file extension.
+                f.write(r.content)
+
