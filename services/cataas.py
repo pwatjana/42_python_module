355948c858851d8c9bec25e963fed837e09f5f46
pwatjana <ppwj10976@gmail.com>
2024-01-30 21:03:39 +0700

    sa

--- services/cataas.py
+++ services/cataas.py
@@ -21,9 +21,9 @@ def download_cats(n: int) -> None:
     cat_ids = get_cat_ids(n)
     with httpx.Client(timeout=10) as client:
         for cat_id in cat_ids:
-            r = client.get(f"http//cataas.com/cat/{cat_id['id']}")
+            r = client.get(f"https://cataas.com/cat/{cat_id['id']}")
             r.raise_for_status()
-            with open(f"out/cats/{cat_id['id']}.{cat_id['ext']}","wb") as f:
+            with open(f"out/(cat_id['id']).(cat_id['ext'])","wb") as f:
                 #open blank folder name out and write file content with name cat ID and file extension.
                 f.write(r.content)
 
